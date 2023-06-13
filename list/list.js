57eabecbd25a528b7e1e03acb165e626ad7957e6
ASAPJulio <juliosarafyan@gmail.com>
2023-06-13 23:45:09 +0200

    add: New Js exercise done => the TodoList

--- list/list.js
+++ list/list.js
@@ -1,1 +1,42 @@
 "use strict"
+
+const formToDoList = document.getElementById('form');
+const todoInput = document.getElementById('todo');
+
+formToDoList.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const todoValue = todo.value;
+
+    let item = `
+                <div class="item">
+                    <p>${todoValue}</p>
+                    <button class="btn-delete"
+                    </button>
+                    <button class="btn-archive">
+                    </button>
+                </div>
+        `
+
+    const listItems = document.querySelector('.list-items');
+    listItems.innerHTML += item;
+
+    const btnDelete = document.querySelectorAll('.btn-delete');
+    btnDelete.forEach(i => {
+        console.log('btn-delete')
+        i.addEventListener('click', function () {
+            console.log('btn-delete cliqué');
+            i.parentElement.remove();
+        });
+    });
+
+    const btnArchive = document.querySelectorAll('.btn-archive');
+    btnArchive.forEach(i => {
+        console.log('btn-archive');
+        i.addEventListener('click', function() {
+            const parent = i.parentElement;
+            parent.classList.toggle('done');
+        });
+    });
+
+    formToDoList.reset();
+});
