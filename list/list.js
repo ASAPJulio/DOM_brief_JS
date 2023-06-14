b1a0217dd2054a2fc7c1f577a385219745d37bed
ASAPJulio <juliosarafyan@gmail.com>
2023-06-14 11:20:07 +0200

    fix: added a condition to avoid adding empty content in the list

--- list/list.js
+++ list/list.js
@@ -7,7 +7,8 @@ formToDoList.addEventListener('submit', function (e) {
     e.preventDefault();
     const todoValue = todoInput.value;
 
-    let item = `
+    if (todoValue.trim() !== '') {
+        let item = `
                 <div class="item">
                     <p>${todoValue}</p>
                     <button class="btn-delete"
@@ -17,26 +18,27 @@ formToDoList.addEventListener('submit', function (e) {
                 </div>
         `
 
-    const listItems = document.querySelector('.list-items');
-    listItems.innerHTML += item;
+        const listItems = document.querySelector('.list-items');
+        listItems.innerHTML += item;
 
-    const btnDelete = document.querySelectorAll('.btn-delete');
-    btnDelete.forEach(i => {
-        console.log('btn-delete')
-        i.addEventListener('click', function () {
-            console.log('btn-delete cliqué');
-            i.parentElement.remove();
+        const btnDelete = document.querySelectorAll('.btn-delete');
+        btnDelete.forEach(i => {
+            console.log('btn-delete')
+            i.addEventListener('click', function () {
+                console.log('btn-delete cliqué');
+                i.parentElement.remove();
+            });
         });
-    });
 
-    const btnArchive = document.querySelectorAll('.btn-archive');
-    btnArchive.forEach(i => {
-        console.log('btn-archive');
-        i.addEventListener('click', function() {
-            const parent = i.parentElement;
-            parent.classList.toggle('done');
+        const btnArchive = document.querySelectorAll('.btn-archive');
+        btnArchive.forEach(i => {
+            console.log('btn-archive');
+            i.addEventListener('click', function () {
+                const parent = i.parentElement;
+                parent.classList.toggle('done');
+            });
         });
-    });
 
+    };
     formToDoList.reset();
 });
